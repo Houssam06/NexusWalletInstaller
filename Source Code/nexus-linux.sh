0577ccb83ff197f76402d65088f2b308d654513f
Alex Sweeney <x2110311x@gmail.com>
2017-01-25 01:45:43 +0000

    Update nexus-linux.sh
    general fixes

--- Source Code/nexus-linux.sh
+++ Source Code/nexus-linux.sh
@@ -1,8 +1,3 @@
-yum_cmd=$("which yum")
-apt-cmd=$(which apt-get)
-rpm_cmd=$(which rpm)
-zypper_cmd=$(which zypper)
-pacman_cmd=$(which pacman)
 sudo_cmd=$(which sudo)
 
 package1="python2.7"
@@ -10,17 +5,19 @@ package2="python-pip"
 package3="libqtgui4"
 package4="unrar"
 
-if [[ ! -e $sudo_cmd ]]; then
-	if [[ ! -e $yum_cmd ]]; then
+if [ -e $sudo_cmd ]; then
+	if [ -x /usr/bin/yum ]; then
 		sudo yum install $package1 $package2 $package3 $package4
-	elif [[ ! -e $apt_cmd ]]; then
+		echo "Yum"
+	elif [ -x /usr/bin/apt-get ]; then
 		sudo apt-get install $package1 $package2 $package3 $package4
-	elif [[ ! -e $rpm_cmd ]]; then
+		echo "apt-get"
+	elif [ -x /usr/bin/rpm ]; then
 		sudo rpm -ivh $package1 $package2 $package3 $package4
-	elif [[ ! -e $zypper_cmd ]]; then
+	elif [ -x /usr/bin/zypper ]; then
 		sudo zypper install $package1 $package2 $package3 $package4
-	elif [[ ! -e $pacman_cmd ]]; then
-		sudo nepacman -s $package1 $package2 $package3 $package4
+	elif [ -x /usr/bin/pacman ]; then
+		sudo pacman -s $package1 $package2 $package3 $package4
 	else
 		echo "error can't install packages"
 		exit 1;
@@ -28,8 +25,9 @@ if [[ ! -e $sudo_cmd ]]; then
 else
 	echo "Please run this script as root or install sudo"
  fi
- 
+
  mkdir ~/Nexus
  cd ~/Nexus
+ pip install --upgrade pip
  wget https://raw.githubusercontent.com/x2110311x/NexusWalletInstaller/master/Source%20Code/Nexus-Linux.py
- python Nexus-Linux.
+python Nexus-Linux.py
