d5158d34181cd99b8cf36e6fe9178aed1720dc14
Alex Sweeney <x2110311x@gmail.com>
2017-01-24 20:00:29 -0500

    Fixed Nexus-Linux.sh and modified Nexus-Linux.py accordingly

--- Source Code/nexus-linux.sh
+++ Source Code/nexus-linux.sh
@@ -1,27 +1,31 @@
-yum_cmd=$(which yum)
+yum_cmd=$("which yum")
 apt-cmd=$(which apt-get)
 rpm_cmd=$(which rpm)
 zypper_cmd=$(which zypper)
 pacman_cmd=$(which pacman)
+sudo_cmd=$(which sudo)
 
 package1="python2.7"
 package2="python-pip"
 package3="libqtgui4"
 package4="unrar"
-
-if [[ ! -e $yum_cmd ]]; then
-    yum install $package1 $package2 $package3 $package4
- elif [[ ! -e $apt_cmd ]]; then
-    apt-get install $package1 $package2 $package3 $package4
- elif [[ ! -e $rpm_cmd ]]; then
-	rpm -ivh $package1 $package2 $package3 $package4
- elif [[ ! -e $zypper_cmd ]]; then
-	zypper install $package1 $package2 $package3 $package4
- elif [[ ! -e $pacman_cmd ]]; then
-	pacman -s $package1 $package2 $package3 $package4
- else
-    echo "error can't install packages"
-    exit 1;
+if if [[ ! -e $sudo_cmd]]; then
+	if [[ ! -e $yum_cmd ]]; then
+		sudo yum install $package1 $package2 $package3 $package4
+	elif [[ ! -e $apt_cmd ]]; then
+		sudo apt-get install $package1 $package2 $package3 $package4
+	elif [[ ! -e $rpm_cmd ]]; then
+		sudo rpm -ivh $package1 $package2 $package3 $package4
+	elif [[ ! -e $zypper_cmd ]]; then
+		sudo zypper install $package1 $package2 $package3 $package4
+	elif [[ ! -e $pacman_cmd ]]; then
+		sudo pacman -s $package1 $package2 $package3 $package4
+	else
+		echo "error can't install packages"
+		exit 1;
+	fi
+else
+	echo "Please run this script as root or install sudo"
  fi
  
  mkdir ~/Nexus
